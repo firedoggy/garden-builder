738674ab7fda4c669b7609a0ee6dcd75bd9eb60c
Samuel <sammkenzie@gmail.com>
2021-02-08 14:26:41 -0600

    Filled out User#new and User#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save 
+            log_in(@user)
+            redirect_to user_path(@user)
+        else 
+            flash.now[:danger] = @user.errors.full_messages
+            render :new 
+        end 
     end
 
     def show
