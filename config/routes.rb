c0dd0a8edeab070142b7cc71bb897c9979711948
Samuel <sammkenzie@gmail.com>
2021-02-08 14:25:12 -0600

    Disabled Webpacker, created signup view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :plants
-  resources :gardens
+
+    get '/signup' => 'users#new', as: 'signup'
+    post '/signup' => 'users#create'
+
+    resources :users
+    resources :plants
+    resources :gardens
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
