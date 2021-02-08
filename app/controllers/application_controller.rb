40afd317eb874f84d262ede0438c58aa666bfb2c
Samuel <sammkenzie@gmail.com>
2021-02-08 14:43:06 -0600

    Added helper methods for sessions/users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,25 @@ class ApplicationController < ActionController::Base
     helper_method :logged_in?
 
     private
-    
+
+    def log_in(user)
+        session[:user_id] = user.id 
+    end 
+
     def logged_in?
         !!session[:user_id]
     end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
+    end 
 end
